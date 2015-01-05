3dbab2a4e9d6a6aea1c3ea3f162fee622e2d89fe
brossetti1 <brossetti1@gmail.com>
2015-01-05 18:39:58 -0500

    finished day 1 homework assignments

--- 01-05/complete/03_NumberGame_complete.rb
+++ 01-05/complete/03_NumberGame_complete.rb
@@ -1,34 +1,28 @@
 
-def check(num)
-  until num.to_i.class == Fixnum
-    if num.class != Fixnum || Float
-     puts "That was not a number, try again: "
-     num = gets.to_i
-   end
- end
-end
-
-
 def guessGame
   puts number = "pick a number between 0 and 100: "
-  num = gets.to_i; puts num.class; check(num);
-  #check = if num.class != Fixnum; puts "That was not a number, try again: "; end
+  num = check(gets).to_i
   randNum = rand(100)
   until num == randNum
     if num < randNum
       puts "#{num} is lower than the number, guess again: "
-      num = gets.to_i; check(num)
+      num = check(gets).to_i
     elsif num > randNum
       puts "#{num} is higher than the number, guess again: "
-      num = gets.to_i; check(num)
+      num = check(gets).to_i
     end
   end
   puts "You got it bro/ladybro, you win, how awesome does that feel?"
 end
 
 
-puts 50.class == Fixnum
-puts 50.class != Fixnum
+def check(num)
+  until /^[\d]+(\.[\d]+){0,1}$/ === num
+    puts "That was not a number, try again: "
+    num = gets
+  end
+  num
+end
+
 
 guessGame
-
